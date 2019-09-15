a6f54846f248d0121f862425bc069c3205d3ba1c
Ying Zhou <yzhouyzhou@yahoo.com>
2019-09-14 20:21:08 -0500

    remove commented statements

--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -1,8 +1,4 @@
 import React from "react";
-// import Thumbnail from "../Thumbnail";
-// import { Container, Row, Col } from "../Grid";
-// import Btn from "../Btn"
-
 
 export function BookList({ children }) {
   return <ul className="list-group">{children}</ul>;
@@ -11,26 +7,3 @@ export function BookList({ children }) {
 export function BookListItem({ children }) {
   return <li className="list-group-item">{children}</li>;
 }
-
-// export function BookListItem({
-//   image = "https://placehold.it/300x300",
-//   description
-// } ) {
-//   return (
-//     <li className="list-group-item">
-//       <Container>
-//         <Row>
-//           <Col size="xs-4 sm-2">
-//             <Thumbnail src={image} />
-//           </Col>
-//           <Col size="xs-8 sm-9">
-//             <p>
-//               <strong>Description: </strong>
-//               {description}
-//             </p>
-//           </Col>
-//         </Row>
-//       </Container>
-//     </li>
-//   );
-// }
